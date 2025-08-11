062d442b59465ba36240c0b940bb5dda4fd00f9b
Alexey Zinchenko <zizann86@yandex.ru>
2025-08-11 22:57:04 +0500

    Диплом второй этап. 5 итерация. checkstyle

--- stats/stats-service/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-service/src/main/java/ru/practicum/controller/StatsController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.HitDto;
 import ru.practicum.StatsDto;
