7351fec24e80be80df4568824f2d25f6058ae67c
Alexey Zinchenko <zizann86@yandex.ru>
2025-08-17 15:40:36 +0500

    Диплом Третий этап. 2 итерация.CheckStyle

--- main-service/src/main/java/ru/practicum/mapper/CommentMapper.java
+++ main-service/src/main/java/ru/practicum/mapper/CommentMapper.java
@@ -3,10 +3,6 @@ package ru.practicum.mapper;
 import ru.practicum.dto.comment.CommentDto;
 import ru.practicum.dto.comment.CreateCommentDto;
 import ru.practicum.model.Comment;
-import ru.practicum.model.Event;
-import ru.practicum.model.User;
-
-import java.time.LocalDateTime;
 
 public class CommentMapper {
 
@@ -18,15 +14,6 @@ public class CommentMapper {
                 comment.getCreated()
         );
     }
-
-   /* public Comment toComment(CreateCommentDto createCommentDto, Event event, User user) {
-        return Comment.builder()
-                .text(createCommentDto.getText())
-                .event(event)
-                .author(user)
-                .created(LocalDateTime.now())
-                .build();
-    }*/
 
     public static Comment toComment(CreateCommentDto createCommentDto) {
         Comment comment = new Comment();
